41e679996e9a9d2e94c626f6b446f44e0f738445
Ali <alichavoshii1372@gmail.com>
2021-01-23 18:55:47 +0330

    Refactore permission Attribute

--- Digikala.Core/Classes/PermissionAttribute.cs
+++ Digikala.Core/Classes/PermissionAttribute.cs
@@ -10,21 +10,21 @@ namespace Digikala.Core.Classes
     public class PermissionAttribute : AuthorizeAttribute, IAuthorizationFilter
     {
         private readonly int _permissionId;
-        private IUserService _userService;
+        private IAccountRepository _accountRepository;
         private IRolePermissionService _rolePermissionService;
         public PermissionAttribute(int permissionId)
         {
             _permissionId = permissionId;
         }
-        
+
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            _userService = (IUserService)context.HttpContext.RequestServices.GetService(typeof(IUserService));
+            _accountRepository = (IAccountRepository)context.HttpContext.RequestServices.GetService(typeof(IAccountRepository));
             _rolePermissionService = (IRolePermissionService)context.HttpContext.RequestServices.GetService(typeof(IRolePermissionService));
             if (context.HttpContext.User.Identity.IsAuthenticated)
             {
                 var userId = context.HttpContext.User.GetUserId();
-                var userRoleId = _userService.GetUserRole(userId).Result;
+                var userRoleId = _accountRepository.GetUserRole(userId).Result;
                 if (!_rolePermissionService.IsRoleHavePermission(_permissionId, userRoleId).Result)
                 {
                     context.Result = new RedirectResult("/Account/Login?returnUrl=" + context.HttpContext.Request.Path);
