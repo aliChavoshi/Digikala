770aa255c6d41ab94add9f6c76f9cc98a28e01b0
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-05-25 19:01:29 +0430

    change shared view names

--- Digikala/Areas/AdminPanel/Controllers/HomeController.cs
+++ Digikala/Areas/AdminPanel/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Digikala.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
+    [Route("[area]/[controller]/[action]")]
     public class HomeController : Controller
     {
         public IActionResult Index()
