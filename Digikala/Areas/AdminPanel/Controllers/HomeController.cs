a7933868a44e9e636053759dda31c0cf945c6dc2
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-06-10 15:04:06 +0430

    add modal for role

--- Digikala/Areas/AdminPanel/Controllers/HomeController.cs
+++ Digikala/Areas/AdminPanel/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Digikala.Core.Interfaces.Identity;
+using Digikala.DataAccessLayer.Entities.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Digikala.Areas.AdminPanel.Controllers
 {
@@ -6,9 +9,27 @@ namespace Digikala.Areas.AdminPanel.Controllers
     [Route("[area]/[controller]/[action]")]
     public class HomeController : Controller
     {
+        private readonly IRoleRepository _roleRepository;
+
+        public HomeController(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Roles()
+        {
+            return View(await _roleRepository.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(string title)
+        {
+            return NotFound();
         }
     }
 }
