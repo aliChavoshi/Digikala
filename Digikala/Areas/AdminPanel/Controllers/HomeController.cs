4e953226416012218af310814713c303d7fbb9c9
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-06-12 14:58:54 +0430

    complete controller Role in admin panel

--- Digikala/Areas/AdminPanel/Controllers/HomeController.cs
+++ Digikala/Areas/AdminPanel/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Threading.Tasks;
+using Digikala.Core.Interfaces;
 using Digikala.Core.Interfaces.Identity;
 using Digikala.DataAccessLayer.Entities.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 
 namespace Digikala.Areas.AdminPanel.Controllers
 {
@@ -10,11 +12,18 @@ namespace Digikala.Areas.AdminPanel.Controllers
     public class HomeController : Controller
     {
         private readonly IRoleRepository _roleRepository;
+        private readonly IAccountRepository _accountRepository;
+        private readonly IRolePermissionRepository _rolePermissionRepository;
 
-        public HomeController(IRoleRepository roleRepository)
+        public HomeController(IRoleRepository roleRepository, IAccountRepository accountRepository, IRolePermissionRepository rolePermissionRepository)
         {
             _roleRepository = roleRepository;
+            _accountRepository = accountRepository;
+            _rolePermissionRepository = rolePermissionRepository;
         }
+
+        #region Role
+
         public IActionResult Index()
         {
             return View();
@@ -26,10 +35,75 @@ namespace Digikala.Areas.AdminPanel.Controllers
             return View(await _roleRepository.ToListAsync());
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateRole(string title)
+        [HttpGet]
+        public IActionResult CreateRole()
         {
-            return NotFound();
+            return PartialView();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(Role model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Roles");
+            }
+            if (await _roleRepository.IsExist(x => x.Title.ToLower() == model.Title.ToLower()))
+            {
+                return RedirectToAction("Roles");
+            }
+            await _roleRepository.Add(model);
+            await _roleRepository.Save();
+            return RedirectToAction("Roles");
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> EditRole(int id)
+        {
+            return PartialView(await _roleRepository.GetById(id));
+        }
+
+        [HttpPost("{id:int}")]
+        public async Task<IActionResult> EditRole(Role model)
+        {
+            var role = await _roleRepository.GetById(model.Id);
+            if (model.Title != role.Title)
+            {
+                if (await _roleRepository.IsExist(x => x.Title.ToLower() == model.Title.ToLower()))
+                {
+                    return RedirectToAction("Roles");
+                }
+            }
+            role.Title = model.Title;
+            _roleRepository.Update(role);
+            await _roleRepository.Save();
+            return RedirectToAction("Roles");
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            return PartialView(await _roleRepository.GetById(id));
+        }
+
+        [HttpPost("{id:int}")]
+        public async Task<IActionResult> DeleteRole(Role model)
+        {
+            if (await _accountRepository.IsExist(x => x.RoleId == model.Id))
+            {
+                return RedirectToAction("Roles");
+            }
+            if (await _rolePermissionRepository.IsExist(x => x.RoleId == model.Id))
+            {
+                return RedirectToAction("Roles");
+            }
+            var role = await _roleRepository.GetById(model.Id);
+            role.IsDeleted = true;
+            _roleRepository.Update(role);
+            await _roleRepository.Save();
+            return RedirectToAction("Roles");
+        }
+
+        #endregion
     }
 }
